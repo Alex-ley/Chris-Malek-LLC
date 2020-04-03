1930bc806669c2d4f99ac8d6a667a2e8a93257f8
Alexander Ley <91.alex.ley@googlemail.com>
2020-04-04 00:14:53 +0200

    added better disableScroll behaviour

--- index.js
+++ index.js
@@ -2,29 +2,58 @@ let rellax = new Rellax('.rellax');
 
 const menu_wrap = document.querySelector('.menu-wrap');
 const burger = document.getElementById('nav-icon1');
+const body = document.querySelector('body');
 burger.addEventListener('click', e => {
     burger.classList.toggle("open");
     menu_wrap.classList.toggle("open");
     if (burger.classList.contains('open')){
-        //disableScroll();
+        disableScroll();
+        body.classList.toggle("open");
     }else{
-        //enableScroll();
+        enableScroll();
+        body.classList.toggle("open");
     }
 })
 
-function disableScroll() { 
-    // Get the current page scroll position 
-    scrollTop =  window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft =  window.pageXOffset || document.documentElement.scrollLeft, 
+// https://stackoverflow.com/a/4770179/9792594:
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1};
 
-    // if any scroll is attempted, 
-    // set this to the previous value 
-    window.onscroll = function(e) { 
-        e.preventDefault();
-        window.scrollTo(scrollLeft, scrollTop); 
-    }; 
-} 
+function preventDefault(e) {
+  e.preventDefault();
+}
 
-function enableScroll() { 
-    window.onscroll = function() {}; 
-} 
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
