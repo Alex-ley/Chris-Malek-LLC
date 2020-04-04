61d9492f75bc57dda5e6a961c208652faae635bb
Alexander Ley <91.alex.ley@googlemail.com>
2020-04-04 23:01:58 +0200

    added contact page with form

---
+++ forms.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll('.input-field > input');
+
+inputs.forEach(input => {
+    input.addEventListener('focus', inputFocus);
+    input.addEventListener('blur', inputBlur);
+})
+
+function inputFocus(e) {
+    //console.log(e.target)
+    const input = e.target;
+    if (!input.classList.contains('has-data')){
+        input.classList.toggle("has-data");
+    }
+}
+function inputBlur(e) {
+    //console.log(e.target)
+    const input = e.target;
+    console.log(input.value.length)
+    if (!input.value.length > 0){
+        input.classList.toggle("has-data");
+    }
+}
